37b1e15a83c5355703413d6303d5e619c225bebd
Gabriel Coimbra De Oliveira Da Silva <gabao.coimbra@gmail.com>
2025-08-01 10:40:14 -0300

    Ajustando para slash commands

--- bot.py
+++ bot.py
@@ -32,56 +32,65 @@ riot_api_key = os.getenv("RIOT_API_KEY")
 
 # Configuração de intenções
 intents = discord.Intents.default()
-intents.message_content = True  # Permite que o bot veja o conteúdo das mensagens
-intents.voice_states = True  # Necessário para o bot de música
+intents.message_content = True
+intents.voice_states = True
+intents.members = True
 
-# Cria uma instância do bot, usando commands.Bot e definindo o prefixo para os comandos
+# Cria uma instância do bot, SEM prefixo de comando
 client = commands.Bot(command_prefix="!", intents=intents)
+# Cria a árvore de comandos para gerenciar os slash commands
+tree = discord.app_commands.CommandTree(client)
 
 
 # Evento de inicialização
 @client.event
 async def on_ready():
+    # Sincroniza os slash commands com o Discord
+    await tree.sync()
     print(f"Bot logado como {client.user}")
     print("------")
 
 
-# Comandos de texto e entretenimento
-@client.command(name="ping")
-async def ping(ctx):
-    await ctx.send("Pong!")
+# --- Comandos de texto e entretenimento (Convertidos para Slash Commands) ---
 
 
-@client.command(name="dica")
-async def dica(ctx):
-    await ctx.send("Lembre-se de sempre comprar wards para ter visão do mapa!")
+@tree.command(name="ping", description="Responde com Pong!")
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message("Pong!")
 
 
-@client.command(name="lol-build")
-async def lol_build(ctx):
-    await ctx.send(
+@tree.command(name="dica", description="Fornece uma dica de League of Legends.")
+async def dica(interaction: discord.Interaction):
+    await interaction.response.send_message(
+        "Lembre-se de sempre comprar wards para ter visão do mapa!"
+    )
+
+
+@tree.command(name="lol-build", description="Sugere uma build para um campeão.")
+async def lol_build(interaction: discord.Interaction):
+    await interaction.response.send_message(
         "Tryndamere: Gume do Infinito, Colhedor de Essência e Mata-Cráquens."
     )
 
 
-@client.command(name="debuia")
-async def debuia(ctx):
-    await ctx.send("Mesmo na derrota vamos debuiaaa!")
+@tree.command(name="debuia", description="Exibe a frase de efeito do time.")
+async def debuia(interaction: discord.Interaction):
+    await interaction.response.send_message("Mesmo na derrota vamos debuiaaa!")
 
 
-@client.command(name="piada")
-async def piada(ctx):
+@tree.command(name="piada", description="Conta uma piada aleatória.")
+async def piada(interaction: discord.Interaction):
     piadas = [
         "O que o tomate foi fazer no banco? Foi tirar um extrato!",
         "O que o pato disse para a pata? Vem Quá!",
         "Por que a aranha é o animal mais inteligente da floresta? Porque ela tem uma teia de ideias!",
     ]
     piada_escolhida = random.choice(piadas)
-    await ctx.send(piada_escolhida)
+    await interaction.response.send_message(piada_escolhida)
 
 
-# Comandos de voz (música)
-# Assegure-se de que o FFmpeg esteja instalado e no PATH do seu sistema.
+# --- Comandos de voz (música) (Convertidos para Slash Commands) ---
+
 ydl_opts = {
     "format": "bestaudio/best",
     "postprocessors": [
@@ -95,36 +104,44 @@ ydl_opts = {
 }
 
 
-@client.command(name="entrar")
-async def entrar(ctx):
-    if not ctx.message.author.voice:
-        await ctx.send(
-            f"{ctx.message.author.name} não está conectado a um canal de voz!"
+@tree.command(name="entrar", description="Conecta o bot ao seu canal de voz.")
+async def entrar(interaction: discord.Interaction):
+    if not interaction.user.voice:
+        await interaction.response.send_message(
+            f"{interaction.user.name} não está conectado a um canal de voz!"
         )
         return
 
-    canal = ctx.message.author.voice.channel
+    canal = interaction.user.voice.channel
     await canal.connect()
-    await ctx.send(f"Conectado ao canal de voz **{canal.name}**.")
+    await interaction.response.send_message(
+        f"Conectado ao canal de voz **{canal.name}**."
+    )
 
 
-@client.command(name="sair")
-async def sair(ctx):
-    if ctx.voice_client:
-        await ctx.guild.voice_client.disconnect()
-        await ctx.send("Desconectado do canal de voz.")
+@tree.command(name="sair", description="Desconecta o bot do canal de voz.")
+async def sair(interaction: discord.Interaction):
+    if interaction.guild.voice_client:
+        await interaction.guild.voice_client.disconnect()
+        await interaction.response.send_message("Desconectado do canal de voz.")
     else:
-        await ctx.send("O bot não está conectado a um canal de voz.")
+        await interaction.response.send_message(
+            "O bot não está conectado a um canal de voz."
+        )
 
 
-@client.command(name="tocar")
-async def tocar(ctx, *, url):
-    if not ctx.voice_client:
-        await ctx.send("O bot não está conectado a um canal de voz.")
+@tree.command(name="tocar", description="Toca uma música do YouTube.")
+async def tocar(interaction: discord.Interaction, url: str):
+    if not interaction.guild.voice_client:
+        await interaction.response.send_message(
+            "O bot não está conectado a um canal de voz."
+        )
         return
 
-    if ctx.voice_client.is_playing():
-        ctx.voice_client.stop()
+    await interaction.response.defer()  # Usado para evitar timeout em operações longas
+
+    if interaction.guild.voice_client.is_playing():
+        interaction.guild.voice_client.stop()
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -132,20 +149,31 @@ async def tocar(ctx, *, url):
             filename = ydl.prepare_filename(info)
 
         source = discord.FFmpegPCMAudio(filename, executable="ffmpeg")
-        ctx.voice_client.play(
+        interaction.guild.voice_client.play(
             source, after=lambda e: print("Tocando música. Erro: %s" % e) if e else None
         )
 
-        await ctx.send(f'Tocando agora: **{info.get("title", "Música")}**')
+        await interaction.followup.send(
+            f'Tocando agora: **{info.get("title", "Música")}**'
+        )
 
     except Exception as e:
-        await ctx.send(f"Ocorreu um erro: {e}")
+        await interaction.followup.send(f"Ocorreu um erro: {e}")
 
 
-# Comandos de integração com a API da Riot
-@client.command(name="historicolol")
-async def lol_match(ctx, *, summoner_name):
+# --- Comandos de integração com a API da Riot (Convertidos para Slash Commands) ---
+
+
+@tree.command(
+    name="historicolol",
+    description="Mostra o histórico das 5 partidas mais recentes de um invocador.",
+)
+async def lol_match(interaction: discord.Interaction, summoner_name: str):
     summoner_url = f"https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}?api_key={riot_api_key}"
+
+    await interaction.response.send_message(
+        f"Buscando as últimas 5 partidas de **{summoner_name}**..."
+    )
 
     try:
         response = requests.get(summoner_url)
@@ -157,8 +185,6 @@ async def lol_match(ctx, *, summoner_name):
         match_history_url = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=5&api_key={riot_api_key}"
         match_history_response = requests.get(match_history_url)
         match_ids = match_history_response.json()
-
-        await ctx.send(f"Buscando as últimas 5 partidas de **{summoner_name}**...")
 
         for match_id in match_ids:
             match_url = f"https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={riot_api_key}"
@@ -174,85 +200,28 @@ async def lol_match(ctx, *, summoner_name):
             champion = jogador["championName"]
             kda = f"{jogador['kills']}/{jogador['deaths']}/{jogador['assists']}"
 
-            await ctx.send(f"**{champion}** - **{outcome}** - KDA: **{kda}**")
-
-    except requests.exceptions.HTTPError as err:
-        if response.status_code == 404:
-            await ctx.send(f"Invocador **{summoner_name}** não encontrado.")
-        else:
-            await ctx.send(f"Ocorreu um erro na requisição à API da Riot: {err}")
-    except Exception as e:
-        await ctx.send(f"Ocorreu um erro: {e}")
-
-
-import requests  # Certifique-se de que requests já está importado
-
-# ... seu código existente ...
-
-
-@client.command(name="lolaovivo")
-async def lol_live(ctx, *, summoner_name):
-    # Primeiro, busca o ID do invocador
-    summoner_url = f"https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}?api_key={riot_api_key}"
-
-    try:
-        response = requests.get(summoner_url)
-        response.raise_for_status()
-        summoner_data = response.json()
-
-        summoner_id = summoner_data["id"]
-
-        # Agora, usa o ID para checar a partida em tempo real
-        live_game_url = f"https://br1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/{summoner_id}?api_key={riot_api_key}"
-        live_game_response = requests.get(live_game_url)
-
-        if live_game_response.status_code == 404:
-            await ctx.send(
-                f"O invocador **{summoner_name}** não está em uma partida ativa."
+            await interaction.channel.send(
+                f"**{champion}** - **{outcome}** - KDA: **{kda}**"
             )
-            return
-
-        live_game_response.raise_for_status()
-        game_data = live_game_response.json()
-
-        # Constrói a mensagem com as informações da partida
-        game_mode = game_data["gameMode"]
-
-        await ctx.send(f"**{summoner_name}** está em uma partida de **{game_mode}**!")
-
-        blue_team = ""
-        red_team = ""
-
-        for participant in game_data["participants"]:
-            player_name = participant["summonerName"]
-            champion_name = participant["championName"]
-
-            # Formata a string para cada time
-            if participant["teamId"] == 100:  # Time Azul
-                blue_team += f" - {player_name} ({champion_name})\n"
-            else:  # Time Vermelho
-                red_team += f" - {player_name} ({champion_name})\n"
-
-        embed = discord.Embed(
-            title=f"Partida em Andamento ({game_mode})", color=discord.Color.blue()
-        )
-        embed.add_field(name="Time Azul", value=blue_team, inline=False)
-        embed.add_field(name="Time Vermelho", value=red_team, inline=False)
-
-        await ctx.send(embed=embed)
 
     except requests.exceptions.HTTPError as err:
         if response.status_code == 404:
-            await ctx.send(f"Invocador **{summoner_name}** não encontrado.")
+            await interaction.channel.send(
+                f"Invocador **{summoner_name}** não encontrado."
+            )
         else:
-            await ctx.send(f"Ocorreu um erro na requisição à API da Riot: {err}")
+            await interaction.channel.send(
+                f"Ocorreu um erro na requisição à API da Riot: {err}"
+            )
     except Exception as e:
-        await ctx.send(f"Ocorreu um erro: {e}")
+        await interaction.channel.send(f"Ocorreu um erro: {e}")
 
 
-@client.command(name="elolol")
-async def lol_rank(ctx, *, summoner_name):
+@tree.command(name="elolol", description="Retorna o elo e rank de um jogador.")
+async def lol_rank(interaction: discord.Interaction, summoner_name: str):
     summoner_url = f"https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}?api_key={riot_api_key}"
+
+    await interaction.response.defer()  # Usado para evitar timeout
 
     try:
         response = requests.get(summoner_url)
@@ -266,7 +235,7 @@ async def lol_rank(ctx, *, summoner_name):
         rank_data = rank_response.json()
 
         if not rank_data:
-            await ctx.send(
+            await interaction.followup.send(
                 f"O invocador **{summoner_name}** não tem rank em filas ranqueadas."
             )
         else:
@@ -277,37 +246,102 @@ async def lol_rank(ctx, *, summoner_name):
             wins = rank_info["wins"]
             losses = rank_info["losses"]
 
-            await ctx.send(
+            await interaction.followup.send(
                 f"**{summoner_name}** está em **{tier} {rank}** com **{lp} LP**. ({wins} Vitórias / {losses} Derrotas)"
             )
 
     except requests.exceptions.HTTPError as err:
         if response.status_code == 404:
-            await ctx.send(f"Invocador **{summoner_name}** não encontrado.")
+            await interaction.followup.send(
+                f"Invocador **{summoner_name}** não encontrado."
+            )
         else:
-            await ctx.send(f"Ocorreu um erro na requisição à API da Riot: {err}")
+            await interaction.followup.send(
+                f"Ocorreu um erro na requisição à API da Riot: {err}"
+            )
     except Exception as e:
-        await ctx.send(f"Ocorreu um erro: {e}")
+        await interaction.followup.send(f"Ocorreu um erro: {e}")
 
 
-# Adicione este comando no seu arquivo bot.py
-@client.command(name="comandos")
-async def comandos(ctx):
+@tree.command(
+    name="lol-live",
+    description="Mostra os jogadores e campeões de uma partida em andamento.",
+)
+async def lol_live(interaction: discord.Interaction, summoner_name: str):
+    summoner_url = f"https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}?api_key={riot_api_key}"
+
+    await interaction.response.defer()  # Usado para evitar timeout
+
+    try:
+        response = requests.get(summoner_url)
+        response.raise_for_status()
+        summoner_data = response.json()
+
+        summoner_id = summoner_data["id"]
+
+        live_game_url = f"https://br1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/{summoner_id}?api_key={riot_api_key}"
+        live_game_response = requests.get(live_game_url)
+
+        if live_game_response.status_code == 404:
+            await interaction.followup.send(
+                f"O invocador **{summoner_name}** não está em uma partida ativa."
+            )
+            return
+
+        live_game_response.raise_for_status()
+        game_data = live_game_response.json()
+
+        game_mode = game_data["gameMode"]
+
+        blue_team = ""
+        red_team = ""
+
+        for participant in game_data["participants"]:
+            player_name = participant["summonerName"]
+            champion_name = participant["championName"]
+
+            if participant["teamId"] == 100:
+                blue_team += f" - {player_name} ({champion_name})\n"
+            else:
+                red_team += f" - {player_name} ({champion_name})\n"
+
+        embed = discord.Embed(
+            title=f"Partida em Andamento ({game_mode})", color=discord.Color.blue()
+        )
+        embed.add_field(name="Time Azul", value=blue_team, inline=False)
+        embed.add_field(name="Time Vermelho", value=red_team, inline=False)
+
+        await interaction.followup.send(embed=embed)
+
+    except requests.exceptions.HTTPError as err:
+        if response.status_code == 404:
+            await interaction.followup.send(
+                f"Invocador **{summoner_name}** não encontrado."
+            )
+        else:
+            await interaction.followup.send(
+                f"Ocorreu um erro na requisição à API da Riot: {err}"
+            )
+    except Exception as e:
+        await interaction.followup.send(f"Ocorreu um erro: {e}")
+
+
+@tree.command(name="comandos", description="Exibe a lista de todos os comandos do bot.")
+async def comandos(interaction: discord.Interaction):
     embed = discord.Embed(
         title="🤖 Comandos do Bot Debuia Team",
         description="Lista de todas as funcionalidades disponíveis.",
         color=discord.Color.gold(),
     )
 
-    # Adicionando os campos de funcionalidades
     embed.add_field(
         name="🎮 Comandos de League of Legends",
         value=(
-            "**`!elolol <nome_do_invocador>`**\n"
+            "**`/elolol <nome>`**\n"
             "   - Retorna o elo e rank de um jogador.\n"
-            "**`!historicolol <nome_do_invocador>`**\n"
+            "**`/historicolol <nome>`**\n"
             "   - Mostra o histórico das 5 partidas mais recentes.\n"
-            "**`!lol-live <nome_do_invocador>`**\n"
+            "**`/lol-live <nome>`**\n"
             "   - Mostra os jogadores e campeões de uma partida em andamento."
         ),
         inline=False,
@@ -316,11 +350,11 @@ async def comandos(ctx):
     embed.add_field(
         name="🎵 Comandos de Música",
         value=(
-            "**`!entrar`**\n"
+            "**`/entrar`**\n"
             "   - Conecta o bot ao seu canal de voz.\n"
-            "**`!sair`**\n"
+            "**`/sair`**\n"
             "   - Desconecta o bot do canal de voz.\n"
-            "**`!tocar <URL>`**\n"
+            "**`/tocar <url>`**\n"
             "   - Toca uma música do YouTube."
         ),
         inline=False,
@@ -329,20 +363,19 @@ async def comandos(ctx):
     embed.add_field(
         name="🎲 Comandos de Entretenimento",
         value=(
-            "**`!ping`**\n"
+            "**`/ping`**\n"
             "   - Responde com `Pong!`.\n"
-            "**`!piada`**\n"
+            "**`/piada`**\n"
             "   - Conta uma piada aleatória.\n"
-            "**`!debuia`**\n"
+            "**`/debuia`**\n"
             "   - Exibe a frase de efeito do time.\n"
-            "**`!lol-build`**\n"
+            "**`/lol-build`**\n"
             "   - Fornece um exemplo de build para um campeão."
         ),
         inline=False,
     )
 
-    # Envia a mensagem embed no canal
-    await ctx.send(embed=embed)
+    await interaction.response.send_message(embed=embed)
 
 
 keep_alive()
